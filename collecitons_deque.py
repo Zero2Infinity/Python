ebeca1c501693065db1d6482223fb0835e8bce39
rahul <rahul@localhost.localdomain>
2019-02-10 22:29:48 -0600

    collection_deque problem solved

--- collecitons_deque.py
+++ collecitons_deque.py
@@ -2,10 +2,17 @@ from collections import deque
 
 if __name__ == '__main__':
     d = deque()
-    lines = []
     for _ in range(int(input())):
-        lines.append('d.{0}({1})'.format(*input().split()+['']))
-        ' unpackaged args list + empty char for pop() syntax to avoid out of index err'
+        eval('d.{0}({1})'.format(*input().split()+['']))
+    ' unpackaged args list + empty char for pop() syntax to avoid out of index err'
         
-    print (lines)
+    print (*d)
 
+'''
+idea: getattr is build class.append(1) or class.pop()
+for _ in range(int(input())):
+    in = input().split()
+    getattr(d, in[0])(*in[1] i len(in) > 1 else [])
+
+print (*[it for it in d])
+'''
