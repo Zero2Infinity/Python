d47f0acafc0bf2ac16c1ce3dc829684785919fda
rahul <rahul@localhost.localdomain>
2019-03-30 10:16:05 -0500

    set_difference problem solved

---
+++ set_difference.py
@@ -0,0 +1,13 @@
+if __name__ == '__main__':
+    n = int(input())
+    setA = set(map(int, input().split()))
+    m = int(input())
+    setB = set(map(int, input().split()))
+    
+    print (len(setA - setB))
+
+
+'''
+_, a, _, b = eval( set(input().split()), "*4); print(len(a|b)
+
+'''
